944adabf93964c2306c62305e597b7cd5665edbe
Jonathan Politzki <91631593+jonathan-politzki@users.noreply.github.com>
2024-08-03 22:09:53 -0400

    new index

---
+++ Model/New_Model.py
@@ -0,0 +1,4 @@
+
+# Not sure why its so hard for me to crowbar these models into the github. Will write out an ideal model
+ 
+# model intakes data in a two column table of text and titles. Only one is needed for this exercise.
